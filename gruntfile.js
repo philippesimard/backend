c714a5186bad80f81687a81e93e4dda9f69b371e
leseulsteve <leseulsteve@gmail.com>
2015-09-27 18:44:10 -0400

    premier commit

---
+++ gruntfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    express: {
+      options: {
+        port: process.env.PORT || 9001
+      },
+      dev: {
+        options: {
+          script: 'index.js'
+        }
+      },
+      prod: {
+        options: {
+          script: 'index.js',
+          'node_env': 'production'
+        }
+      }
+    },
+
+    watch: {
+      express: {
+        files: [
+          'index.js',
+          'src/{,*//*}*.{js,json}'
+        ],
+        tasks: ['express:dev'],
+        options: {
+          livereload: 35728,
+          nospawn: true //Without this option specified express won't be reloaded
+        }
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      }
+    },
+
+    maildev: {
+      run: {
+        keepAlive: true,
+        open: true
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        'src/{,*/}*.js'
+      ]
+    },
+
+    shell: {
+      mongo: {
+        command: 'mongod --dbpath db --smallfiles',
+        options: {
+          async: true
+        }
+      }
+    },
+  });
+
+  grunt.registerTask('dev', ['shell:mongo', 'express:dev', 'maildev', 'watch']);
+};
