7dd161fb672a20b442530bcacff5251439a5c9f1
leseulsteve <leseulsteve@gmail.com>
2015-11-21 14:30:54 -0500

    no message

--- config/config.js
+++ config/config.js
@@ -25,7 +25,7 @@ module.exports = {
 
 	mongoose: {
 
-		URI: process.env.mongoURI || 'mongodb://localhost/allies'
+		URI: process.env.mongoURI ||  'mongodb://localhost/allies'
 
 	},
 
@@ -68,7 +68,16 @@ module.exports = {
 			secret: '7279BEE6EBCC80400E2CED8D12D0591D34EA5C5F3B3D557A1773F1680F217780',
 		},
 		apiRoot: 'api/v1',
-		unprotectedRoutes: ['api/v1/parcours/*','api/v1/media/*', 'api/v1/media-section/*', 'api/v1/niveau/*', 'api/v1/piste/*', 'api/v1/secteur/*']
+		unprotectedRoutes: [
+			'api/v1/parcours/*',
+			'api/v1/media/*',
+			'api/v1/media-section/*',
+			'api/v1/niveau/*',
+			'api/v1/piste/*',
+			'api/v1/secteur/*',
+			'api/v1/activite/*',
+			'api/v1/partenaire/*'
+		]
 	},
 
 	mailer: {
